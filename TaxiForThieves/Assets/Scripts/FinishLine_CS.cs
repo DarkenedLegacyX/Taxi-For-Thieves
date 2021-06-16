87efb40592233f43e6ae3942df1796388a12365b
DarkenedLegacyX <65576363+DarkenedLegacyX@users.noreply.github.com>
2021-06-16 15:13:00 +1000

    Finish Line Merge

---
+++ TaxiForThieves/Assets/Scripts/FinishLine_CS.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine_CS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (LevelManager_CS.instance.playerhasCrim == true)
+            {
+                print("Criminal Dropped off!");
+                LevelManager_CS.instance.playerhasCrim = false;
+                GameUI_CS.instance.haveCrim = false;
+                GameUI_CS.instance.UpdateUI();
+                //gameObject.transform.position = new Vector3(0, -25, 0);
+            }
+            else
+            {
+                print("Pick up dem Crims!");
+            }
+        }
+    }
+}

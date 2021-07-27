2beb13f5979c380d276a2f6501a87a59ba90afbf
DarkenedLegacyX <65576363+DarkenedLegacyX@users.noreply.github.com>
2021-07-28 03:32:44 +1000

    Radar map Animation + World Building
    - Changed Map frame, added in animation, plays when player picks up and drops off criminal.
    -World building, finished the roads.
    -Tidied up the HUD.
    -Moved spawn points.
    -increase radius on criminal and drop point.

--- TaxiForThieves/Assets/Scripts/RotateRadar.cs
+++ TaxiForThieves/Assets/Scripts/RotateRadar.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class RotateRadar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject radar;
+    private Animator animator;
+
     void Start()
     {
-        
+        animator = radar.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(0,0,1 * Time.deltaTime);
+        //this.transform.Rotate(0,0,1 * Time.deltaTime);
+        if(LevelManager_CS.instance.playerhasCrim == true)
+        {
+            print("Test");
+            animator.SetBool("HasCrim", true);
+        }
+        else
+        {
+            animator.SetBool("HasCrim", false);
+
+        }
     }
 }
